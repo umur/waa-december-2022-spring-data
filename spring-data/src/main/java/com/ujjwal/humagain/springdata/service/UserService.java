8d6cbcf538142228faaaa2dc2b72ba4462f4ef9d
deStarxis <humagainuzjwol@gmail.com>
2022-12-01 20:07:43 -0600

    added services of all domains and their implementations

--- spring-data/src/main/java/com/ujjwal/humagain/springdata/service/UserService.java
+++ spring-data/src/main/java/com/ujjwal/humagain/springdata/service/UserService.java
@@ -1,4 +1,13 @@
 package com.ujjwal.humagain.springdata.service;
 
+import com.ujjwal.humagain.springdata.entity.dto.UserDto;
+
+import java.util.List;
+
 public interface UserService {
+    void save(UserDto userDto);
+    void update(int id, UserDto userDto);
+    void delete(int id);
+    List<UserDto> findAll();
+    UserDto findById(int id);
 }
